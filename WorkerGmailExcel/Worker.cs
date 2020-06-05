a857c47c25c2566856edfe6cdd4f7424a8486375
Francisco Ballesteros Fernandez <francisco.ballesteros@qosit.eu>
2020-06-05 11:32:27 +0200

    worker de correo operativo

--- WorkerGmailExcel/Worker.cs
+++ WorkerGmailExcel/Worker.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Mail;
 using System.Threading;
@@ -26,8 +27,6 @@ namespace WorkerGmailExcel
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-
                 using (var client = new ImapClient())
                 {
                     try
@@ -35,43 +34,42 @@ namespace WorkerGmailExcel
                         using (var cancel = new CancellationTokenSource())
                         {
                             client.Connect("imap.gmail.com", 993, true, cancel.Token);
-
-                            // If you want to disable an authentication mechanism,
-                            // you can do so by removing the mechanism like this:
-                            client.AuthenticationMechanisms.Remove("XOAUTH");
-
                             client.Authenticate("cacharreos2020@gmail.com", "Cacharreos.2020", cancel.Token);
 
-                            // The Inbox folder is always available...
+                            // Miramos al inbox del correo.
                             var inbox = client.Inbox;
                             inbox.Open(FolderAccess.ReadOnly, cancel.Token);
 
                             Console.WriteLine("Total messages: {0}", inbox.Count);
                             Console.WriteLine("Recent messages: {0}", inbox.Recent);
 
-                            // download each message based on the message index
+                            // Descargamos los mensajes que se encuentren en el index
                             for (int i = 0; i < inbox.Count; i++)
                             {
+                                inbox.Open(FolderAccess.ReadWrite);
+
                                 var message = inbox.GetMessage(i, cancel.Token);
                                 Console.WriteLine("Asunto: {0}", message.Subject);
                                 Console.WriteLine("ResentDate: {0}", message.ResentDate);
                                 Console.WriteLine("Fecha: {0}", message.Date.LocalDateTime);
                                 if (message.Attachments.Count() != 0)
                                 {
-                                    Console.WriteLine("Adjunto: {0}", message.Attachments.FirstOrDefault().IsAttachment);
-                                    MimeEntity adjunto = message.Attachments.FirstOrDefault();
+                                    foreach (var att in message.Attachments)
+                                    {
+                                        if (!File.Exists(@"C:\Users\" + Environment.UserName + "\\Downloads\\" + att.ContentType.Name))
+                                        {
+                                            //Función de crear
+                                            CrearArchivo(att);
+                                        }
+                                        else
+                                        {
+                                            File.Delete(@"C:\Users\" + Environment.UserName + "\\Downloads\\" + att.ContentType.Name);
+                                            //Función de crear
+                                            CrearArchivo(att);
+                                        }
+                                    }                                    
                                 }
-                            }
-
-                            // let's try searching for some messages...
-                            var query = SearchQuery.DeliveredAfter(DateTime.Parse("2013-01-12"))
-                                .And(SearchQuery.SubjectContains("MailKit"))
-                                .And(SearchQuery.Seen);
-
-                            foreach (var uid in inbox.Search(query, cancel.Token))
-                            {
-                                var message = inbox.GetMessage(uid, cancel.Token);
-                                Console.WriteLine("[match] {0}: {1}", uid, message.Subject);
+                                inbox.AddFlags(i, MessageFlags.Seen, true);
                             }
 
                             client.Disconnect(true, cancel.Token);
@@ -84,6 +82,25 @@ namespace WorkerGmailExcel
 
                     await Task.Delay(1000, stoppingToken);
                 }
+            }
+        }
+
+        private static void CrearArchivo(MimeEntity att)
+        {
+            using (FileStream stream = File.Create(@"C:\Users\" + Environment.UserName + "\\Downloads\\" + att.ContentType.Name))
+            {
+                if (att is MessagePart)
+                {
+                    MessagePart rfc822 = (MessagePart)att;
+                    rfc822.Message.WriteTo(stream);
+                }
+                else
+                {
+                    var part = (MimePart)att;
+                    part.Content.DecodeTo(stream);
+                }
+
+                FileInfo fileInfo = new FileInfo(stream.Name);
             }
         }
     }
